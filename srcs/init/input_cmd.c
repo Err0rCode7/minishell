3be1dec7ad252ce950e948b9c9247b382e54f9cb
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-03-22 01:04:08 +0900

    fix prompt overlap error

--- srcs/init/input_cmd.c
+++ srcs/init/input_cmd.c
@@ -6,7 +6,7 @@
 /*   By: taewan <taewan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 00:01:49 by taewan            #+#    #+#             */
-/*   Updated: 2022/03/22 00:13:04 by taewan           ###   ########.fr       */
+/*   Updated: 2022/03/22 01:01:53 by taewan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*get_cmd(char **buf)
 	tmp = getcwd(0, MAX_DIRLEN);
 	if (tmp == NULL)
 		exit(1);
-	prompt = ft_strjoin(tmp, " \x1b[32m$\x1b[0m ");
+	prompt = ft_strjoin(tmp, " $ ");
 	free(tmp);
 	if (!prompt)
 		exit(1);
