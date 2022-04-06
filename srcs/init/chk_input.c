8f555f1a3a53155d5ee102765bb74fec39c7c015
Err0rCode7 <aa6184708@naver.com>
2022-04-07 01:26:51 +0900

    Fix: quote new line bug

--- srcs/init/chk_input.c
+++ srcs/init/chk_input.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   chk_input.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taewan <taewan@student.42.fr>              +#+  +:+       +#+        */
+/*   By: seujeon <seujeon@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 00:01:42 by taewan            #+#    #+#             */
-/*   Updated: 2022/03/22 00:01:44 by taewan           ###   ########.fr       */
+/*   Updated: 2022/04/07 01:26:32 by seujeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ static int	chk_is_valid(char *str)
 		return (FALSE);
 	if (args.flag_sq || args.flag_bq)
 	{
-		ft_putstr_fd(MSG_QUOTE_ERR, STDERR);
+		ft_putendl_fd(MSG_QUOTE_ERR, STDERR);
 		return (FALSE);
 	}
 	return (TRUE);
@@ -75,6 +75,7 @@ int	pre_process_input(char *str)
 	if (!(chk_is_valid(str)))
 	{
 		free(str);
+		g_exit_status = 1;
 		return (FALSE);
 	}
 	return (TRUE);
